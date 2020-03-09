8afae5231072e9d5cdb75d27c2f3cb02120174e8
benson516 <benson516@hotmail.com>
2020-03-09 21:36:51 +0800

    Update model.py

--- model.py
+++ model.py
@@ -42,11 +42,19 @@ y_train = np.array(measurements)
 #--------------------------------------#
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
+from Keras.layers.convolutional import Convolution2D
+from Keras.layer.pooling import MaxPooling2D
 
 # Create the model
 model = Sequential()
 model.add( Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)) )
+model.add( Convolution2D(6,5,5,activation='relu') )
+model.add( MaxPooling2D())
+model.add( Convolution2D(16,5,5,activation='relu') )
+model.add( MaxPooling2D())
 model.add( Flatten() )
+model.add( Dense(120) )
+model.add( Dense(84) )
 model.add( Dense(1) )
 
 model.compile( loss='mse', optimizer='adam' )
