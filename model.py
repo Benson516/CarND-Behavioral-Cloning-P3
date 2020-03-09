3c030e3ecc4e09828b3e58d0edad31dd7ad8a0c7
BensonHuang <BensonHuang@itri.org.tw>
2020-03-09 17:53:05 +0800

    Add model.py

---
+++ model.py
@@ -0,0 +1,51 @@
+import csv
+import cv2
+import numpy as np
+from  scipy import ndimage
+
+data_path = '/opt/carnd_p3/data/'
+
+# Data importing
+lines = []
+with open(data_path + 'driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+for idx, line in enumerate(lines):
+    if idx == 0:
+        continue
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = data_path + '/IMG/' + filename
+    # image = cv2.imread(current_path)
+    image = ndimage.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+print("Finish loading data")
+    
+# Trainning data
+#--------------------------------------#
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+# Train network
+#--------------------------------------#
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+
+# Create the model
+model = Sequential()
+model.add( Flatten( input_shape=(160,320,3) ) )
+model.add( Dense(1) )
+
+model.compile( loss='mse', optimizer='adam' )
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=4)
+
+# Save the model
+model.save('model.h5')
+
+
