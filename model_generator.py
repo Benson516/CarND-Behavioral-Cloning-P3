69eaae68afd9cd4494698e5d4ff06c2f136fbc7f
benson516 <benson516@hotmail.com>
2020-03-10 17:31:06 +0800

    Add cropping layer

--- model_generator.py
+++ model_generator.py
@@ -80,14 +80,16 @@ valid_gen = generator(validation_samples, batch_size=batch_size, data_path=data_
 # Train network
 #--------------------------------------#
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, LeakyReLU
+from keras.layers import Flatten, Dense, Lambda, LeakyReLU, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 # Create the model
 #--------------------------------------#
 model = Sequential()
-model.add( Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)) )
+model.add( Cropping2D(cropping=((50,20), (0,0)), input_shape=(row, col, ch) ) )
+# model.add( Lambda( lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)) )
+model.add( Lambda( lambda x: x / 255.0 - 0.5 ) )
 model.add( Convolution2D(6,5,5,activation=None) )
 model.add( LeakyReLU(alpha=0.2) )
 model.add( MaxPooling2D())
